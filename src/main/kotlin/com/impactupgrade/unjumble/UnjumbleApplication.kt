600ca71f8a9080788dca3db397096e54c390f4b0
Brett Meyer <brett@3riverdev.com>
2020-07-19 13:38:45 -0400

    initial commit from Spring Initialzr

---
+++ src/main/kotlin/com/impactupgrade/unjumble/UnjumbleApplication.kt
@@ -0,0 +1,11 @@
+package com.impactupgrade.unjumble
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UnjumbleApplication
+
+fun main(args: Array<String>) {
+	runApplication<UnjumbleApplication>(*args)
+}
---
+++ src/test/kotlin/com/impactupgrade/unjumble/UnjumbleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.impactupgrade.unjumble
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnjumbleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
